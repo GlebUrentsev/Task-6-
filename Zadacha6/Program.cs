cefee3e461555efa93d4181c9c3525d801c2dcd3
Gleb <Gleb-urencev@yandex.ru>
2019-05-10 18:02:08 +0300

    Recursion added

--- Zadacha6/Program.cs
+++ Zadacha6/Program.cs
@@ -8,15 +8,26 @@ namespace Zadacha6
 {
     class Program
     {
+        static int position;
         static bool okey = false; // неубывающаяя/возрастает,если false
         static bool neMonoton = true;
         static void printMas(double[] array)//метод для печати элементов последовательности
         {
-            for (int i = 0; i < array.Length; i++)
+            if (array.Length == 0) return;
+            else
             {
-                Console.WriteLine($"{i + 1} элемент последовательности равен " + array[i]);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1} элемент последовательности равен " + array[i]);
+                }
             }
         } // печать последовательности
+        static double Element(double[] array, int i) //получение элементов через рекурсию
+        {
+            if (i - 1 == array.Length) return array[i - 1];
+            else 
+                return array[i] = 0.7 * array[i - 1] - 0.2 * array[i - 2] + i * array[i - 3];
+        }
         static void checkMas(double[] array)// выбираем нечётные элементы из последовательности и проверяем на монотонность
         {         
             int masLength=0;
@@ -27,19 +38,19 @@ namespace Zadacha6
                     masLength++;
                 }
                 else continue;
-            }
-            double[] massiv = new double[masLength];
+            } //кол-во нечётных индексов в массиве
+            double[] massiv = new double[masLength];//массив нечётных индексов
             int b = 0;
             for (int i = 1; i < array.Length + 1; i++)
             {
-                if (i % 2 != 0)
+                if (i % 2 != 0)// выбираем чётные элменты в массив
                 {
-                    //massiv[i - 1] = array[i - 1];
                     massiv[b] = array[i-1];
                     b++;
                 }
                 else continue;
             }
+            printMas(massiv);
             for(int i=1;i<massiv.Length;i++)
             {
                 if (massiv[i-1]<massiv[i])
@@ -62,19 +73,45 @@ namespace Zadacha6
             Console.Write("Введите элемент a2 = "); a2 = int.Parse(Console.ReadLine());
             Console.Write("Введите элемент a3 = "); a3 = int.Parse(Console.ReadLine());
             Console.Write("Введите количество элементов последовательности N = "); N = int.Parse(Console.ReadLine());
-            double[] mas = new double[N];
-            mas[0] = a1;
-            mas[1] = a2;
-            mas[2] = a3;
-            for (int i = 3; i < N; i++)
+            try
             {
-                mas[i] = 0.7 * mas[i - 1] - 0.2 * mas[i - 2] + i * mas[i - 3];// массив последовательности
+                double[] mas = new double[N];
+                mas[0] = a1;
+                mas[1] = a2;
+                mas[2] = a3;
+                //for (int i = 3; i < N; i++)
+                //{
+                //    mas[i] = 0.7 * mas[i - 1] - 0.2 * mas[i - 2] + i * mas[i - 3];// массив последовательности
+                //}
+                position = 3;
+                while (position < mas.Length)
+                {
+                    Element(mas, position);//массив заданных элементов, индекс элмента с какого начинам получать остальные     
+                    position++;
+                }
+                printMas(mas);
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Массив элементов с нечётными индексами");
+                Console.ResetColor();
+                checkMas(mas);
             }
-            checkMas(mas);         
-            printMas(mas);
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Неверный размер массива");
+            }
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Определение монотонности");
+            Console.ResetColor();       
             if (okey == false && neMonoton == false) Console.WriteLine("Монотонная неубывающая (возрастает)");
-            else if (okey == true && neMonoton == false)  Console.WriteLine("Монотонная убывающая (убывает)");
+            else if (okey == true && neMonoton == false) Console.WriteLine("Монотонная убывающая (убывает)");
             else Console.WriteLine("Не монотонная");
+            Console.ReadKey();
         }
     }
 }
